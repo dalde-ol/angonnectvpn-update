72eefb368e92420bd6d35a8e65fb6766082f9a9d
Dalson De Oliveira <dalson.deoliveira.5@gmail.com>
2026-05-24 20:44:44 +0100

    Script for update

---
+++ bump_version.ps1
@@ -0,0 +1,16 @@
+﻿param (
+    [Parameter(Mandatory = $true)]
+    [string]$Version
+)
+
+$repoPath = "C:\Users\dalso\Downloads\angonnectvpn-update"
+Set-Location $repoPath
+
+"{ ""min_version"": ""$Version"" }" | Out-File -FilePath version.json -Encoding utf8
+
+git add version.json
+git commit -m "bump min_version to $Version"
+git push origin main
+
+Write-Host ""
+Write-Host "min_version actualizado para $Version" -ForegroundColor Green
